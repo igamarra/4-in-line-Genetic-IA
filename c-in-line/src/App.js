22072ea7a5c6152e5493bf9051fa24c51e2ff45f
Ignacio Gamarra <ignacio.gamarra@almundo.com>
2021-03-27 20:16:53 -0300

    first commit

---
+++ c-in-line/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import FormHelperText from "@material-ui/core/FormHelperText";
+
+function App() {
+  return (
+      <FormControl>
+        <InputLabel htmlFor="my-input">Email address</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+      </FormControl>
+  );
+}
+
+export default App;
